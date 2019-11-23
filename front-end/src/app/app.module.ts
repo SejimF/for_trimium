9be0186aab5df1d6fdf1b9a46e410910f8c821ab
Колодько Максим <kolodkomaxim@gmail.com>
2019-11-23 12:57:26 +0300

    Implemented the basic layout.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SocialNetworkModule } from './components/social-network/social-network.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialNetworkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
